b68eb2845395cc0ae57ad4872e567eaed98417d0
thepercentageguy <chica33sfm@gmail.com>
2022-09-02 21:19:18 +0200

    f

--- src/Main.hx
+++ src/Main.hx
@@ -1,12 +1,25 @@
 package ;
 
+import haxe.ui.components.Label;
+import haxe.ui.macros.ComponentMacros;
+import haxe.ui.core.Component;
 import haxe.ui.HaxeUIApp;
 
 class Main {
+    static var args:Array<String> = Sys.args();
+
     public static function main() {
         var app = new HaxeUIApp();
+
         app.ready(function() {
-            app.addComponent(new MainView());
+            var mainView:Component = ComponentMacros.buildComponent("assets/main-view.xml");
+            app.addComponent(mainView);
+
+            var errorLabel:Label = mainView.findComponent("errorLabel", Label);
+            errorLabel.text = "ERROR: " + args[0];
+
+            var stacktraceLabel:Label = mainView.findComponent("stacktraceLabel", Label);
+            stacktraceLabel.text = args[1];
 
             app.start();
         });
